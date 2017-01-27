d258ddc35739ed19284cc27ef821466e5cde8fe7
austin.drenski <austin.drenski@gmail.com>
2017-01-27 17:19:35 -0500

    Added test program from old solution file.

---
+++ AD.OpenXml.Tests/AD.OpenXml.Tests/Demo.cs
@@ -0,0 +1,53 @@
+﻿using AD.IO;
+using AD.OpenXml.Documents;
+
+namespace AD.OpenXml.Tests
+{
+    public static class DemoProgram
+    {
+        public static void Demo()
+        {
+            // Declare working directory
+            const string path = @"G:\data\austin d\508 programming\508 presentation to oars-ocio (1-25-17)\";
+
+            // Declare template and source file
+            DocxFilePath source = DocxFilePath.Create(path + "Introduction example.docx");
+
+            // Create result file from template
+            DocxFilePath result = DocxFilePath.Create(path + "Result - Introduction example.docx", true);
+
+            //// Declare template and source file
+            //DocxFilePath source = DocxFilePath.Create(path + "Testing examples.docx");
+
+            //// Create result file from template
+            //DocxFilePath result = DocxFilePath.Create(path + "Result - Testing example.docx", true);
+
+            // Clean document.xml
+            result.Process508From(source);
+
+            // Create custom styles
+            result.AddStyles();
+
+            // Add headers
+            result.AddHeaders("Year in Trade");
+
+            // Add footers
+            result.AddFooters();
+
+            // Set all chart objects inline
+            result.PositionChartsInline();
+
+            // Set the inner positions of chart objects
+            result.PositionChartsInner();
+
+            // Set the outer positions of chart objects
+            result.PositionChartsOuter();
+
+            // Set the style of bar chart objects
+            result.ModifyBarChartStyles();
+
+            // Set the style of line chart objects
+            result.ModifyLineChartStyles();
+        }
+    }
+}
---
+++ AD.OpenXml.Tests/AD.OpenXml.Tests/Program.cs
@@ -0,0 +1,55 @@
+﻿using AD.IO;
+using AD.OpenXml.Documents;
+
+namespace AD.OpenXml.Tests
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Declare working directory
+            //const string path = @"c:\users\adren\desktop\508 work\";
+            const string path = @"g:\data\austin d\508 programming\508 work\";
+
+            // Declare template and source file
+            DocxFilePath source = DocxFilePath.Create(path + "TestWordDocument.docx");
+
+            // Create result file from template
+            DocxFilePath result = DocxFilePath.Create(path + "TestWordDocument_out.docx", true);
+
+            // Clean document.xml
+            result.Process508From(source);
+
+            // Create custom styles
+            result.AddStyles();
+
+            // Add headers
+            result.AddHeaders("Year in Trade");
+            
+            // Add footers
+            result.AddFooters();
+
+            // Set all chart objects inline
+            result.PositionChartsInline();
+
+            // Set the inner positions of chart objects
+            result.PositionChartsInner();
+
+            // Set the outer positions of chart objects
+            result.PositionChartsOuter();
+
+            // Set the style of bar chart objects
+            result.ModifyBarChartStyles();
+
+            // Set the style of line chart objects
+            result.ModifyLineChartStyles();
+
+            // Test AltChunk implementation
+            //result.ReadAsXml().Elements().First().AddAltChunk(result, source).SaveInto(result, "word/document.xml");
+
+            // Write document.xml to XML file
+            XmlFilePath xml = XmlFilePath.Create(path + "TestWordDocument_out.xml");
+            result.ReadAsXml().Elements().WriteXml(xml);
+        }
+    }
+}
