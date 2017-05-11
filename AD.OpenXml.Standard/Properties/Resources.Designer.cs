3020c98cc55a2856984bed35508673099f7b7042
austin.drenski <austin.drenski@gmail.com>
2017-05-11 15:47:27 -0400

    Updated to run on a .NET Standard 1.6 library and a .NET Framework 4.7 console application.

--- AD.OpenXml.Standard/Properties/Resources.Designer.cs
+++ AD.OpenXml.Standard/Properties/Resources.Designer.cs
@@ -38,7 +38,7 @@ namespace AD.OpenXml.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AD.OpenXml.Standard.Properties.Resources", typeof(Resources).GetTypeInfo().Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("AD.OpenXml.Properties.Resources", typeof(Resources).GetTypeInfo().Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
