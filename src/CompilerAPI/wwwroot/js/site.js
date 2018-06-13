5b71a5a8b516e4aa7ed1d9c07c03f2f048e3ebdc
Austin Drenski <austin.drenski@gmail.com>
2018-06-13 09:45:23 -0400

    Updates project files

---
+++ src/CompilerAPI/wwwroot/js/site.js
@@ -0,0 +1,11 @@
+﻿$(".custom-file-input").change(function () {
+    const n = document.getElementById("file-names");
+    for (let t; t = n.firstChild;) n.removeChild(t);
+    const t = this.files;
+    for (let i = 0; i < t.length; i++) {
+        const r = document.createElement("li");
+        r.appendChild(document.createTextNode(t[i].name));
+        n.appendChild(r)
+    }
+    $(this).next().after().text(`${t.length} files selected`)
+});
